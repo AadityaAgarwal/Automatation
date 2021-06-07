271eed29dcf85aa8f6b320e38e6885499be1ae23
AadityaAgarwal <73215428+AadityaAgarwal@users.noreply.github.com>
2021-06-07 21:41:40 +0530

    Update AutomaticTyping.py

--- AutomaticTyping.py
+++ AutomaticTyping.py
@@ -1,11 +1,15 @@
+# I request the users to please keep atleast one tab in google chrome open so that the program can work smoothly. :)
+
 from platform import python_branch
 import pyautogui
 screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
 currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
-pyautogui.moveTo(800, 1100) # Move the mouse to XY coordinates.
-pyautogui.click()
-pyautogui.hotkey('ctrl','t')
+pyautogui.hotkey('ctrl','alt','c')
 pyautogui.moveTo(300,50)
 pyautogui.click()
 pyautogui.write('https://web.whatsapp.com/')
 pyautogui.press('enter')
+pyautogui.hotkey('ctrl','t')
+pyautogui.write('https://code.whitehatjr.com/s/dashboard')
+pyautogui.press('enter')
+pyautogui.moveTo(300,300)
